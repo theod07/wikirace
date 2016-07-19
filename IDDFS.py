af2e885eed951f3b259c3077eb74bfce68418182
theod07 <theodore.do@gmail.com>
2016-07-19 11:23:38 -0700

    filled out functions IDDFS(), DLS(). need to test

--- IDDFS.py
+++ IDDFS.py
@@ -1,16 +1,30 @@
 def IDDFS(root):
     """
     DOCSTRING PLACEHOLDER
+    Reference:
+    https://en.wikipedia.org/wiki/Iterative_deepening_depth-first_search#Algorithm
     """
-    pass
+    for depth in xrange(3):
+        found = DLS(start, depth)
+        if found:
+            return found
 
 
 def DLS(node, depth):
     """
     DOCSTRING PLACEHOLDER
+    Reference:
+    https://en.wikipedia.org/wiki/Iterative_deepening_depth-first_search#Algorithm
     """
-    pass
+    if depth == 0:
+        return node
+    elif depth > 0:
+        for child in node.child_nodes:
+            found = DLS(child, depth-1)
+            return found
+    else:
+        print 'Depth of search cannot be negative.'
+    return
 
 
 if __name__ == '__main__':
-    
