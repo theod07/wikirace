e306a6d48b563dbeb968ff3775774f9d1d74fb37
theod07 <theodore.do@gmail.com>
2016-07-19 11:51:17 -0700

    DLS can find nodes. need to test IDDFS

--- IDDFS.py
+++ IDDFS.py
@@ -1,10 +1,12 @@
+from wiki_node import wiki_node as wnode
+
 def IDDFS(root):
     """
     DOCSTRING PLACEHOLDER
     Reference:
     https://en.wikipedia.org/wiki/Iterative_deepening_depth-first_search#Algorithm
     """
-    for depth in xrange(3):
+    for depth in xrange(2):
         found = DLS(start, depth)
         if found:
             return found
@@ -16,15 +18,24 @@ def DLS(node, depth):
     Reference:
     https://en.wikipedia.org/wiki/Iterative_deepening_depth-first_search#Algorithm
     """
+    print 'depth   : ', depth
+    print 'node.url: ', node.url
     if depth == 0:
-        return node
+        print 'depth = 0'
+        # print node.url
+        # return node
     elif depth > 0:
+        print len(node.child_nodes)
         for child in node.child_nodes:
-            found = DLS(child, depth-1)
-            return found
+            child_node = wnode(child)
+            found = DLS(child_node, depth-1)
+            # return found
     else:
         print 'Depth of search cannot be negative.'
     return
 
 
 if __name__ == '__main__':
+    start_node = wnode('https://en.wikipedia.org/wiki/Iterative_deepening_depth-first_search')
+
+    print DLS(start_node, 2)
