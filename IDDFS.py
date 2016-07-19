4a922487049e6eb4cc16c9030d21b95573ed7dbb
theod07 <theodore.do@gmail.com>
2016-07-19 10:40:59 -0700

    create skeleton for IDDFS.py file

---
+++ IDDFS.py
@@ -0,0 +1,16 @@
+def IDDFS(root):
+    """
+    DOCSTRING PLACEHOLDER
+    """
+    pass
+
+
+def DLS(node, depth):
+    """
+    DOCSTRING PLACEHOLDER
+    """
+    pass
+
+
+if __name__ == '__main__':
+    
