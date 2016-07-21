4a95a63d8a8520844bbb5c75a3534e4ae4174aff
theod07 <theodore.do@gmail.com>
2016-07-20 19:41:25 -0700

    update bash script to take in JSON object from command line

--- IDDFS.py
+++ IDDFS.py
@@ -1,6 +1,8 @@
 from wiki_node import wiki_node as wnode
 import itertools
 import time
+import json
+import sys
 
 def IDDFS(start, end):
     """
@@ -9,12 +11,10 @@ def IDDFS(start, end):
     https://en.wikipedia.org/wiki/Iterative_deepening_depth-first_search#Algorithm
     """
     for depth in itertools.count():
-        print 'depth: ', depth
         found = DLS([start], end, depth)
 
         if found:
             return found
-
 
 def DLS(route, end, depth):
     """
@@ -27,7 +27,6 @@ def DLS(route, end, depth):
 
     current = route[-1]
     if current == end:
-        print 'current == end'
         return route
 
     for child in wnode(current).child_nodes:
@@ -40,7 +39,29 @@ def DLS(route, end, depth):
         return 'Depth cannot be negative.'
 
 
-if __name__ == '__main__':
+def test_cases():
+    """
+    Test out some start and end articles.
+    This search should require depth=0
+    Sample output:
+        depth:  0
+        depth:  1
+        current == end
+        ['https://en.wikipedia.org/wiki/Malaria']
+        tdelta :  7.10487365723e-05
+        This search should require depth=1
+        depth:  0
+        depth:  1
+        depth:  2
+        current == end
+        ['https://en.wikipedia.org/wiki/Malaria', 'https://en.wikipedia.org/wiki/Agriculture']
+        tdelta :  74.5454380512
+        This search should require depth=2
+        depth:  0
+        depth:  1
+        depth:  2
+        depth:  3
+    """
     start = 'https://en.wikipedia.org/wiki/Malaria'
     end0 =  'https://en.wikipedia.org/wiki/Malaria'
     end1 = 'https://en.wikipedia.org/wiki/Agriculture'
@@ -71,18 +92,10 @@ if __name__ == '__main__':
         print IDDFS(start, end3)
         print 'tdelta : ', time.time() - tstart
 
-#
-# In [35]: run IDDFS.py
-# depth:  0
-# depth:  1
-# depth:  2
-# current == end
-# ['https://en.wikipedia.org/wiki/Iterative_deepening_depth-first_search', 'https://en.wikipedia.org/wiki/Time_complexity']
-# tdelta1 :  16.2023248672
-# depth:  0
-# depth:  1
-# depth:  2
-# depth:  3
-# current == end
-# ['https://en.wikipedia.org/wiki/Iterative_deepening_depth-first_search', 'https://en.wikipedia.org/wiki/Backtracking', 'https://en.wikipedia.org/wiki/Algorithm']
-# tdelta2 :  271.779721022
+if __name__ == '__main__':
+
+    print 'This is the name of the script: ', sys.argv[0]
+    print 'Number of arguments: ', len(sys.argv)
+
+    json_obj = json.loads(sys.argv[1])
+    print IDDFS(json_obj['start'], json_obj['end'])
